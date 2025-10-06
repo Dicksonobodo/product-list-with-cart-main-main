ce570bc85455de5062aa4cd853e7d5633df86ca7
Your Name <you@example.com>
2025-10-06 04:55:26 +0100

    fouth commit

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ import express from "express";
 import bodyParser from "body-parser";
 import pg from "pg";
 import bcrypt from "bcrypt";
-import crypto from "crypto";
 import nodemailer from "nodemailer";
 import dotenv from "dotenv";
 
